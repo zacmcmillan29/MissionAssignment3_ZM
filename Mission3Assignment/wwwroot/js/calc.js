2a85646c800d01ddc8b47e7e1e5a9151e1996a36
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2022-01-17 18:30:51 -0700

    initial upload

---
+++ Mission3Assignment/wwwroot/js/calc.js
@@ -0,0 +1,147 @@
+﻿
+// javascript function
+
+//document.getElementById("add").addEventListener('click', function () {
+//    var n1 = document.getElementById("assignment").value;
+//    var n1Weight = (parseInt(n1) * .55);
+
+//    var n2 = document.getElementById("group").value;
+//    var n2Weight = (parseInt(n2) * .05);
+
+
+//    var n3 = document.getElementById("quiz").value;
+//    var n3Weight = (parseInt(n3) * .1);
+
+//    var n4 = document.getElementById("exam").value;
+//    var n4Weight = (parseInt(n4) * .2);
+
+//    var n5 = document.getElementById("intex").value;
+//    var n5Weight = (parseInt(n5) * .1);
+
+//    var total = n1Weight + n2Weight + n3Weight + n4Weight + n5Weight;
+//    //alert(total);
+
+//    var letterGrade = "";
+
+//// This is the If statement for the letter grades!
+  
+//    if (total > 94) {
+//        letterGrade = "A"
+//    }
+//    else if (total <= 93 && total > 90) {
+//        letterGrade = "A-"
+//    }
+//    else if (total <= 89 && total > 86) {
+//        letterGrade = "B+"
+//    }
+//    else if (total <= 86 && total > 83) {
+//        letterGrade = "B"
+//    }
+//    else if (total <= 83 && total > 80) {
+//        letterGrade = "B-"
+//    }
+//    else if (total <= 79 && total > 76) {
+//        letterGrade = "C+"
+//    }
+//    else if (total <= 76 && total > 73) {
+//        letterGrade = "C"
+//    }
+//    else if (total <= 73 && total > 70) {
+//        letterGrade = "C-"
+//    }
+//    else if (total <= 69 && total > 66) {
+//        letterGrade = "D+"
+//    }
+//    else if (total <= 66 && total > 63) {
+//        letterGrade = "D"
+//    }
+//    else if (total <= 63 && total > 60) {
+//        letterGrade = "D-"
+//    }
+//    else {
+//        letterGrade = "E"
+//    }
+
+
+
+// alert("The total is: " + total + "%, which is a " + letterGrade);
+
+//    var output = ("The total is: " + total + "%, which is a " + letterGrade);
+//    document.getElementById("output").innerHTML = output;
+//})
+
+
+
+
+
+// need to write the jQuery here
+
+$("#add").click(function () {
+        var n1 = $("#assignment").val();
+        var n1Weight = (parseInt(n1) * .55);
+
+        var n2 = $("#group").val();
+        var n2Weight = (parseInt(n2) * .05);
+
+
+        var n3 = $("#quiz").val();
+        var n3Weight = (parseInt(n3) * .1);
+
+        var n4 = $("#exam").val();
+        var n4Weight = (parseInt(n4) * .2);
+
+        var n5 = $("#intex").val();
+        var n5Weight = (parseInt(n5) * .1);
+
+        var total = n1Weight + n2Weight + n3Weight + n4Weight + n5Weight;
+
+
+
+        var letterGrade = "";
+
+        // This is the If statement for the letter grades!
+
+        if (total > 94) {
+            letterGrade = "A"
+        }
+        else if (total <= 93 && total > 90) {
+            letterGrade = "A-"
+        }
+        else if (total <= 89 && total > 86) {
+            letterGrade = "B+"
+        }
+        else if (total <= 86 && total > 83) {
+            letterGrade = "B"
+        }
+        else if (total <= 83 && total > 80) {
+            letterGrade = "B-"
+        }
+        else if (total <= 79 && total > 76) {
+            letterGrade = "C+"
+        }
+        else if (total <= 76 && total > 73) {
+            letterGrade = "C"
+        }
+        else if (total <= 73 && total > 70) {
+            letterGrade = "C-"
+        }
+        else if (total <= 69 && total > 66) {
+            letterGrade = "D+"
+        }
+        else if (total <= 66 && total > 63) {
+            letterGrade = "D"
+        }
+        else if (total <= 63 && total > 60) {
+            letterGrade = "D-"
+        }
+        else {
+            letterGrade = "E"
+        }
+
+
+
+        alert("The total is: " + total + "%, which is a " + letterGrade);
+
+        var output = ("The total is: " + total + "%, which is a " + letterGrade);
+        $("#output").html(output);
+    })
---
+++ Mission3Assignment/wwwroot/js/calculateGrade.js
@@ -0,0 +1,27 @@
+﻿//One of them needs to be commented out //
+
+
+//HERE is the normal JavaScript//
+
+
+
+document.getElementById("btnSend").addEventListener('click', function () {
+    alert("Assignments: " + document.getElementById("from").value +
+        ", Group Projects: " + document.getElementById("subject").value +
+        ", Quizzes: " + document.getElementById("message").value);
+})
+
+
+
+
+//here is the jQuery shortcut//
+
+//$("#btnSend").click(function () {
+//    alert("Assignments: " + $("#assignments").val() +
+//        ", Group Project: " + $("#group").val() +
+//        ", Quizzes: " + $("#quiz").val() +
+//        ", Exams: " + $("#exam").val() +
+//        ", INTEX: " + $("#intex").val() +
+
+//        );
+//})
